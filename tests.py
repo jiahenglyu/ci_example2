785eb105e71a039851ec5a3744041690adb8a41f
jiahenglyu <55993480+jiahenglyu@users.noreply.github.com>
2020-05-25 13:48:15 -0700

    test

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import task
 
+
 class TestCase(unittest.TestCase):
 
     def test1(self):
@@ -9,7 +10,7 @@ class TestCase(unittest.TestCase):
 
     def test2(self):
         expected = "Goodbye World"
-        self.assertEqual(task.my_func(),expected)
+        self.assertEqual(task.my_func(), expected)
 
 
 if __name__ == '__main__':
